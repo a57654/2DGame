42fa8eedcbbf1282bf4c3773a534b56aff792029
a57654 <a57654@alunos.ipb.pt>
2025-04-15 17:52:47 +0100

    Ajuste velocidade Frame

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
 
-
     // Start is called before the first frame update
     void Start()
     {
+
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
         MoveAction.Enable();
     }
 
@@ -21,7 +23,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
 }
