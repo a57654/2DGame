7eda99ec5045789bc03eb6fc5ad65e9df820623c
a57654 <a57654@alunos.ipb.pt>
2025-04-15 17:45:56 +0100

    Controle Personagem
    Melhoria do Controle do personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,45 +4,24 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        float vertical = 0.0f;
-        Vector2 position = transform.position;
-
-        //Faz o movimento horizontal
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-        Debug.Log(horizontal);
-
-        //Faz o movimento vertical
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
-        
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
