ea2a4a1bf042ed1cd3832c977a460f91f41b9b56
a57654 <a57654@alunos.ipb.pt>
2025-05-06 15:36:08 +0100

    Adição Projetil

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,42 +5,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables related to player character movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
     public float speed = 3.0f;
 
 
-    // Variables related to the health system
     public int maxHealth = 5;
     int currentHealth;
     public int health { get { return currentHealth; } }
 
 
-    // Variables related to temporary invincibility
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
 
 
-    // Variables related to animation
     Animator animator;
     Vector2 moveDirection = new Vector2(1, 0);
 
 
-    // Start is called before the first frame update
+    public GameObject projectilePrefab;
+
+
+
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
-
         currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
@@ -64,9 +61,16 @@ public class PlayerController : MonoBehaviour
             if (damageCooldown < 0)
                 isInvincible = false;
         }
+
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
     }
 
-    // FixedUpdate has the same call rate as the physics system
+
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -89,6 +93,16 @@ public class PlayerController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(moveDirection, 300);
+
+
+        animator.SetTrigger("Launch");
     }
 
 }
